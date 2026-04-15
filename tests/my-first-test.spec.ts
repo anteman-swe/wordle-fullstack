438931282ce7f095422f8cc0c522d9745fffadad
Anders Eriksson <anteman.swe@gmail.com>
2026-04-15 19:08:12 +0200

    Removed tests that doesnt work

--- tests/my-first-test.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('kontrollera titeln på en sida', async ({ page }) => {
-  // Gå till en webbsida
-  await page.goto('https://playwright.dev/');
-
-  // Kontrollera att titeln innehåller ordet "Playwright"
-  await expect(page).toHaveTitle(/Playwright/);
-});
