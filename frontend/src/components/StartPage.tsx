2ee1b81c5a210980146694baf230e2fee80ae643
Anders Eriksson <anteman.swe@gmail.com>
2026-04-17 22:43:51 +0200

    Adjusted Footer placement

--- frontend/src/components/StartPage.tsx
+++ frontend/src/components/StartPage.tsx
@@ -1,12 +1,13 @@
+import '../styles/StartPage.scss';
 import type { ReactNode } from "react";
 import Introtext from "./Introtext";
 import StartGameBtn from "./StartGameBtn";
 
 export default function StartPage(): ReactNode {
   return (
-    <>
+    <div className='startpage'>
       <Introtext />
       <StartGameBtn />
-    </>
+    </div>
   );
 }
