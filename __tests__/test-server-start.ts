a028c26e5168c47fc0860f6d9baf238553504da8
Anders Eriksson <anteman.swe@gmail.com>
2026-04-17 11:48:32 +0200

    Update test-server-start

--- __tests__/test-server-start.ts
+++ __tests__/test-server-start.ts
@@ -1,4 +1,4 @@
-import start from "../backend/src/start-server.js";
+import start from "../backend/server/start-server.js";
 const testWordlist: string[] = ["bästa", "testa"];
 
 export default function testServerStart(list: string[]) {
