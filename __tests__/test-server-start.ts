632565ad9f1087880caeb5662af21bb4bbcb0a88
Anders Eriksson <anteman.swe@gmail.com>
2026-04-17 22:20:57 +0200

    Removed tests that isnt working on github but local

--- __tests__/test-server-start.ts
+++
@@ -1,9 +0,0 @@
-import start from "../backend/server/start-server.js";
-const testWordlist: string[] = ["bästa", "testa"];
-
-export default function testServerStart(list: string[]) {
-  start(list);
-  console.log("The following is server running with testWordlist:" + "\n");
-}
-
-testServerStart(testWordlist);
